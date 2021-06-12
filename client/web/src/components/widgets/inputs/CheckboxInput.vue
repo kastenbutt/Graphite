f631bd901f43486a3109c1ae6ac8a05f7d94ca90
Keavon Chambers <keavon@keavon.com>
2021-06-12 12:03:54 -0700

    Add the checkbox input widget

---
+++ client/web/src/components/widgets/inputs/CheckboxInput.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="checkbox-input">
+		<input type="checkbox" :id="" />
+		<label></label>
+	</div>
+</template>
+
+<style lang="scss">
+.checkbox-input {
+	input {
+		margin: 0;
+		padding: 0;
+		border: none;
+		outline: none;
+		border-radius: 2px;
+		width: 16px;
+		height: 16px;
+	}
+}
+</style>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	id: "",
+	componentId: 0,
+	created() {
+		this.componentId += 1;
+	},
+	props: {},
+	components: {},
+});
+</script>
